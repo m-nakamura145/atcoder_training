5a25edbf5e25c3dc32ea60451bafc248dfd1a3c8
m-nakamura145 <masato.nakamura145@gmail.com>
2018-03-12 23:57:39 +0900

    Add abc090_a

---
+++ abc090_a/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char a[3][3];
+    scanf("%s%s%s", a[0], a[1], a[2]);
+    cout << a[0][0] << a[1][1] << a[2][2] << endl;
+    return 0;
+}
