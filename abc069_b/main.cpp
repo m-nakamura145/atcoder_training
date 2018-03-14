8a5c528989a1026cd13849ffc6db0c28732a4ac2
m-nakamura145 <masato.nakamura145@gmail.com>
2018-03-14 23:46:29 +0900

    Add abc069_b

---
+++ abc069_b/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int size = s.size();
+    cout << s[0] << size-2 << s[size-1] << endl;
+    return 0;
+}
