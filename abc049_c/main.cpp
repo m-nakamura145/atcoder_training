bdd7acb391ca4665cd02cb8cf1d1ac62a6c974c7
m-nakamura145 <masato.nakamura145@gmail.com>
2018-03-18 23:07:13 +0900

    Add abc049_c

---
+++ abc049_c/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string divide[4] = {"dream", "dreamer", "erase", "eraser"};
+    string s;
+    cin >> s;
+
+    reverse(s.begin(), s.end());
+    for(int i=0;i<4;i++) reverse(divide[i].begin(), divide[i].end());
+
+    bool can = true;
+    for(int i=0;i<s.size();){
+        bool can2 = false;
+        for(int j=0;j<4;j++){
+            string d = divide[j];
+            if(s.substr(i, d.size()) == d){
+                can2 = true;
+                i += d.size();
+            }
+        }
+        if(!can2){
+            can = false;
+            break;
+        }
+    }
+    if(can) cout << "YES" << endl;
+    else cout << "NO" << endl;
+
+    return 0;
+}
