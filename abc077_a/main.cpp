b7016ef8e5139c4a2573787c05e78403aca6957f
m-nakamura145 <masato.nakamura145@gmail.com>
2018-04-06 21:18:11 +0900

    Add abc077_a

---
+++ abc077_a/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a,b;
+    cin >> a;
+    cin >> b;
+    if (a[0] == b[2] && a[1] == b[1] && a[2] == b[0]){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
